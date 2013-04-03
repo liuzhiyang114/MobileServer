06866e47e7effef54d4534c7f15a3a492cf491e8
wangjuehui <wangjuehui@192.168.16.181>
2013-04-04 00:54:26 +0800

    Signed-off-by: wangjuehui <wangjuehui@192.168.16.181>

--- src/com/smartlab/net/grizzly/DataBaseServer.java
+++ src/com/smartlab/net/grizzly/DataBaseServer.java
@@ -2,21 +2,34 @@ package com.smartlab.net.grizzly;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.logging.Logger;
 
+import org.glassfish.grizzly.filterchain.Filter;
+import org.glassfish.grizzly.filterchain.FilterChain;
 import org.glassfish.grizzly.filterchain.FilterChainBuilder;
+import org.glassfish.grizzly.filterchain.FilterChainContext;
+import org.glassfish.grizzly.filterchain.FilterChainEvent;
+import org.glassfish.grizzly.filterchain.NextAction;
 import org.glassfish.grizzly.filterchain.TransportFilter;
 import org.glassfish.grizzly.nio.transport.TCPNIOTransport;
 import org.glassfish.grizzly.nio.transport.TCPNIOTransportBuilder;
+import org.glassfish.grizzly.threadpool.ThreadPoolConfig;
 import org.glassfish.grizzly.utils.StringFilter;
 
 public class DataBaseServer {
 
+	
 	private static final Logger logger = Logger.getLogger(DataBaseServer.class.getName());
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		//启动为ZigBee端服务线程
+		ServerForZigBee serverForZigBeeThread=new ServerForZigBee();
+		serverForZigBeeThread.start();
 		
 		// Create a FilterChain using FilterChainBuilder
         FilterChainBuilder filterChainBuilder = FilterChainBuilder.stateless();
@@ -40,7 +53,9 @@ public class DataBaseServer {
 
         try {
             // binding transport to start listen on certain host and port
+
             transport.bind("10.84.199.239", 7776);// IP为实验室的局域网IP
+
 
             // start the transport
             transport.start();
@@ -64,4 +79,71 @@ public class DataBaseServer {
 
 	}
 
+}
+class ServerForZigBee extends Thread{
+	
+	private static final Logger logger = Logger.getLogger(ServerForZigBee.class.getName());
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		super.run();
+		//启动对ZigBee端的服务
+		// Create a FilterChain using FilterChainBuilder
+		FilterChainBuilder filterChainBuilder = FilterChainBuilder.stateless();
+
+		// Add TransportFilter, which is responsible
+		// for reading and writing data to the connection
+		filterChainBuilder.add(new TransportFilter());
+
+		// StringFilter is responsible for Buffer <-> String conversion
+//	        filterChainBuilder.add(new StringFilter(Charset.forName("UTF-8")));
+		filterChainBuilder.add(new CToJavaMessageFilter());
+		
+		// EchoFilter is responsible for echoing received messages
+		filterChainBuilder.add(new ServerForZigBeeFilter());
+		
+		final TCPNIOTransportBuilder builder = TCPNIOTransportBuilder.newInstance(); 
+		final ThreadPoolConfig config = builder.getWorkerThreadPoolConfig(); 
+		config.setCorePoolSize(50).setMaxPoolSize(200).setQueueLimit(1000).setKeepAliveTime(10, TimeUnit.SECONDS); 
+		builder.setServerConnectionBackLog(1000);
+		final TCPNIOTransport transport = builder.build();
+		// Create TCP transport
+//	        final TCPNIOTransport transport =
+//	                TCPNIOTransportBuilder.newInstance().build();
+		
+		
+		transport.setIOStrategy(org.glassfish.grizzly.strategies.WorkerThreadIOStrategy.getInstance());
+		transport.setServerSocketSoTimeout(10);
+		
+//	        transport.setKernelThreadPoolConfig(ThreadPoolConfig.defaultConfig().setCorePoolSize(100).setMaxPoolSize(400).setQueueLimit(500));
+		
+		transport.setProcessor(filterChainBuilder.build());
+		
+		try {
+		    // binding transport to start listen on certain host and port
+		    transport.bind("localhost", 7775);
+
+		    // start the transport
+		    transport.start();
+
+		    logger.info("Press any key to stop the ServerForZigBeeThread...");
+		    System.in.read();
+		    //System.out.println(ms);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+		    logger.info("Stopping ServerForZigBeeThread...");
+		    // stop the transport
+		    try {
+				transport.stop();
+				logger.info("Stopped ServerForZigBeeThread...");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
 }
